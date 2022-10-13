b51634c39e6b6894688b7a8ceee2f0980356ff3f
Maxim Kravchenko <max.kravchenko.dev@gmail.com>
2022-10-13 09:48:44 +0300

    Refactor

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -8,6 +8,7 @@ public class Oscillator : MonoBehaviour
     [SerializeField] Vector3 movementVector;
     float movementFactor;
     [SerializeField] float period = 2f;
+    [SerializeField] float movementOffset = 1f;
     
     // Start is called before the first frame update
     void Start()
@@ -22,7 +23,7 @@ public class Oscillator : MonoBehaviour
         float cycles = Time.time / period; // continually growing over time
         const float tau = Mathf.PI * 2; // constant value of 6.283
         float rawSinWave = Mathf.Sin(cycles * tau); // going from -1 to 1
-        movementFactor = (rawSinWave + 1f) / 2f; // recalculated to go from 0 to 1 so its cleaner  
+        movementFactor = (rawSinWave + movementOffset) / 2f; // recalculated to go from 0 to 1 so its cleaner  
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPosition + offset;
     }
